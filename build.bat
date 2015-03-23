e53a984ccaaaff0460373f1ba2061d8d423d678e
Jeisson Hidalgo <jeissonh@gmail.com>
2015-03-23 18:08:34 -0400

    Batch file generates images for installer, removes Qt language files, and check for errors. Added project images to the installer and some minor improvements.

--- build.bat
+++ build.bat
@@ -7,30 +7,51 @@ rem   Inkscape
 rem   Doxygen
 
 :png
-rem inkscape -f assets/game_logo.svg -C -e assets/game_logo-32x32.png
+echo Exporting vector images to png...
+inkscape -w 200 -h 270 -f assets/game_menu/robot.svg -C -e assets/game_menu/robot.png
+inkscape -w 32 -h 32 -f assets/game_logo.svg -C -e assets/game_logo.png
+rem inkscape -h 64 -f assets/game_title.svg -C -e assets/game_title.png
 
-:build
-mkdir build
+:makefile
+echo Generating Makefile...
+if not exist build mkdir build
 qmake -makefile -o build/Makefile botNeumann.pro
+if %ERRORLEVEL% NEQ 0 goto error
+
+:compile
+echo Compiling botNeumann...
 cd build
 mingw32-make release
+if %ERRORLEVEL% NEQ 0 goto error
 cd ..
 
+:installer-contents
 rem Copy runtime libraries. See http://doc.qt.io/qt-5/windows-deployment.html
-mkdir installer\windows
-copy /y build\release\botNeumann.exe installer\windows
+if not exist installer\windows mkdir installer\windows
+xcopy /q /y build\release\botNeumann.exe installer\windows
 windeployqt -release installer\windows\botNeumann.exe
-
-:doc
-doxygen doc/api.doxygen
+if %ERRORLEVEL% NEQ 0 goto error
+del /q installer\windows\qt*.qm
 
 :installer
 builder-cli build installer/botNeumann.xml --license installer/license.xml --setvars project.outputDirectory=installer
+if %ERRORLEVEL% NEQ 0 goto error
 
-goto end
+goto success
 
+:doc
+doxygen doc/api.doxygen
+if %ERRORLEVEL% NEQ 0 goto error
+goto success
 
 :clean
-rem rm -rf assets/*.png doc/api build/
+del /q /s assets/*.png build/ doc/api installer/windows
+
+:success
+echo Build successful
+goto end
+
+:error
+echo Build failed
 
 :end
