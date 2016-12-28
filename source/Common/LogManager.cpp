34d761e3f813e990a9ab3e505643c01d32c632f9
Jeisson Hidalgo <jeissonh@gmail.com>
2016-12-28 15:17:55 -0600

    Opens and closes a text file for logging. The name of the log file uses the hostname and creation date in order to avoid collisions with other logs when uploaded to the shared server

--- source/Common/LogManager.cpp
+++ source/Common/LogManager.cpp
@@ -1,18 +1,45 @@
 #include "LogManager.h"
+#include <QDateTime>
+#include <QDir>
+#include <QHostInfo>
+#include <QMutex>
+#include <QSettings>
+#include <QStandardPaths>
+#include <QTextStream>
+
+QFile LogManager::logFile;
 
 LogManager::LogManager(QObject* parent)
 	: QObject(parent)
 {
-	qInstallMessageHandler(LogManager::messageHandler);
+	// Open the log file for appending events
+	const QString& filename = getLogFilename();
+	logFile.setFileName(filename);
+	if ( logFile.open(QFile::Append | QFile::Text) )
+	{
+		// The file was opened successfully, install the message handler that will use it
+		qInstallMessageHandler(LogManager::messageHandler);
+		qInfo("Using log file '%s'", qUtf8Printable(filename));
+	}
+	else
+		qWarning("Error: could not open log file '%s'", qUtf8Printable(filename));
 }
 
 LogManager::~LogManager()
 {
 	qInstallMessageHandler(nullptr);
+	logFile.close();
 }
 
 void LogManager::messageHandler(QtMsgType type, const QMessageLogContext& context, const QString& message)
 {
+	// Avoid two or more threads writing on the log file symultaneously
+	static QMutex mutex;
+	QMutexLocker lock(&mutex);
+
+	/// A convenience object to format the text that will be sent to the logFile
+	QTextStream logStream(&logFile);
+
 	QByteArray localMsg = message.toLocal8Bit();
 	switch (type) {
 	case QtDebugMsg:
@@ -31,4 +58,32 @@ void LogManager::messageHandler(QtMsgType type, const QMessageLogContext& contex
 	  fprintf(stderr, "Fatal: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
 	  abort();
 	}
+}
+
+QString LogManager::getLogFilename()
+{
+	// If log filename is in settings, use it, otherwise generate one
+	const QVariant& filename = QSettings().value("Log/Filename");
+	return filename.isNull() ? buildLogFilename(true) : filename.toString();
+}
+
+QString LogManager::buildLogFilename(bool saveInSettings)
+{
+	// Build a log filename using the hostname and the creation date to avoid collisions with other
+	// logs when they are submitted to the shared network server
+	const QString& dirPath = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
+	const QString& hostname = QHostInfo().localHostName();
+	const QString& datetime = QDateTime::currentDateTime().toString("yyMMdd_hhmmss");
+	const QString& filename = QString("%1/bn-%2-%3.log").arg(dirPath).arg(hostname).arg(datetime);
+
+	QDir dir( dirPath );
+	if ( ! dir.exists() )
+		if ( ! dir.mkpath( "." ) )
+			qWarning() << "Unable to create application data directory: " << dir.path();
+
+	// Save the file name on the settings for future use
+	if ( saveInSettings )
+		QSettings().setValue("Log/Filename", filename);
+
+	return filename;
 }
