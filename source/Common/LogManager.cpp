250381abd0f7851490a4a16a831377afbb92d67d
Jeisson Hidalgo <jeissonh@gmail.com>
2016-12-28 19:57:32 -0600

    Log messages are appended to log text file

--- source/Common/LogManager.cpp
+++ source/Common/LogManager.cpp
@@ -1,4 +1,7 @@
 #include "LogManager.h"
+#include "BotNeumannApp.h"
+#include "Player.h"
+
 #include <QDateTime>
 #include <QDir>
 #include <QHostInfo>
@@ -9,17 +12,31 @@
 
 QFile LogManager::logFile;
 
+// Text versios of QtMsgType
+const char* const msgTypeStr[] = { "Debug", "Warning", "Critical", "Fatal", "Info" };
+
 LogManager::LogManager(QObject* parent)
 	: QObject(parent)
 {
 	// Open the log file for appending events
 	const QString& filename = getLogFilename();
 	logFile.setFileName(filename);
+	bool exists = QFile::exists(filename);
 	if ( logFile.open(QFile::Append | QFile::Text) )
 	{
-		// The file was opened successfully, install the message handler that will use it
+		// The file was opened successfully. Install the message handler that will use it
 		qInstallMessageHandler(LogManager::messageHandler);
-		qInfo("Using log file '%s'", qUtf8Printable(filename));
+
+		// Log file is ready to receive messages. If it is a new file save a header
+		if ( exists )
+			qInfo("Appending to log file '%s'", qUtf8Printable(filename));
+		else
+		{
+			// It is a new log file, write the CSV header line and the first event
+			logFile.write("Date;Time;User;Type;Category;Details\n");
+			qInfo("Log file '%s' created", qUtf8Printable(filename));
+		}
+
 	}
 	else
 		qWarning("Error: could not open log file '%s'", qUtf8Printable(filename));
@@ -31,33 +48,34 @@ LogManager::~LogManager()
 	logFile.close();
 }
 
+
 void LogManager::messageHandler(QtMsgType type, const QMessageLogContext& context, const QString& message)
 {
+	// Convert the type to a string
+	Q_ASSERT(type < sizeof(msgTypeStr) / sizeof(msgTypeStr[0]));
+	const char* typeStr = msgTypeStr[type];
+
+	// Write the date and time when the event was logged
+	const QString& datetime = QDateTime::currentDateTime().toString("yyMMdd;hhmmss");
+
+	// Store the id of the current player who generated the event
+	Player* player = BotNeumannApp::getInstance()->getCurrentPlayer();
+	const QByteArray& playerId = player ? player->getId() : QByteArray();
+
 	// Avoid two or more threads writing on the log file symultaneously
 	static QMutex mutex;
 	QMutexLocker lock(&mutex);
 
-	/// A convenience object to format the text that will be sent to the logFile
+	// A convenience object to format the text that will be sent to the logFile
 	QTextStream logStream(&logFile);
+	logStream << datetime << ';' << playerId << ';' << typeStr << ';' << context.category << ';' << message << '\n';
 
-	QByteArray localMsg = message.toLocal8Bit();
-	switch (type) {
-	case QtDebugMsg:
-	  fprintf(stderr, "Debug: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
-	  break;
-	case QtInfoMsg:
-	  fprintf(stderr, "Info: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
-	  break;
-	case QtWarningMsg:
-	  fprintf(stderr, "Warning: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
-	  break;
-	case QtCriticalMsg:
-	  fprintf(stderr, "Critical: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
-	  break;
-	case QtFatalMsg:
-	  fprintf(stderr, "Fatal: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
-	  abort();
-	}
+	// Also copy the message to the standard error
+	QTextStream stderrStream(stderr);
+	stderrStream << typeStr << ": " << context.category << ": " << message << '\n';
+
+	if ( type == QtFatalMsg )
+		abort();
 }
 
 QString LogManager::getLogFilename()
@@ -74,7 +92,7 @@ QString LogManager::buildLogFilename(bool saveInSettings)
 	const QString& dirPath = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
 	const QString& hostname = QHostInfo().localHostName();
 	const QString& datetime = QDateTime::currentDateTime().toString("yyMMdd_hhmmss");
-	const QString& filename = QString("%1/bn-%2-%3.log").arg(dirPath).arg(hostname).arg(datetime);
+	const QString& filename = QString("%1/log-%2-%3.csv").arg(dirPath).arg(hostname).arg(datetime);
 
 	QDir dir( dirPath );
 	if ( ! dir.exists() )
