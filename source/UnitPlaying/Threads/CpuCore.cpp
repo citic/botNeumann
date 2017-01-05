739284f87398aafff61f67e1f883ea32ff4a999a
Jeisson Hidalgo <jeissonh@gmail.com>
2017-01-04 19:03:59 -0600

    Standard input middle asset replaced by Standard output one, because its size was wrong. CpuCore.cpp file was missing in previous commit.

--- source/UnitPlaying/Threads/CpuCore.cpp
+++ source/UnitPlaying/Threads/CpuCore.cpp
@@ -1,6 +1,10 @@
 #include "CpuCore.h"
 #include "Prop.h"
 #include "Scene.h"
+#include "Unit.h"
+
+// The number of memory rows reserved for the robot
+const double robotRows = 2.0;
 
 CpuCore::CpuCore(int cpuCoreNumber, Unit& unit, Scene* scene)
 	: MemorySegment(unit, scene, Qt::Vertical)
@@ -11,6 +15,11 @@ CpuCore::CpuCore(int cpuCoreNumber, Unit& unit, Scene* scene)
 
 CpuCore::~CpuCore()
 {
+}
+
+double CpuCore::getHeightInRows() const
+{
+	return unit.getStackSegmentVisibleRows() + robotRows;
 }
 
 void CpuCore::buildCpuCore()
