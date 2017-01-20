6506bf624d8f94e93de5f8e7f499604ead0816be
Jeisson Hidalgo <jeissonh@gmail.com>
2017-01-20 13:51:46 -0600

    Compiling options optimized for GDB

--- tools/GdbTest/examples/ftoc_f.c
+++ tools/GdbTest/examples/ftoc_f.c
@@ -4,8 +4,10 @@ int main()
 {
 	double fahrenheit = 0.0, celsius = 0.0;
 	printf("Temperature (Fahrenheit): ");
-	scanf("%lf", &fahrenheit);
-	celsius = 5.0 / 9.0 * (fahrenheit - 32.0);
-	printf("%lg Fahrenheit == %lg Celsius\n", fahrenheit, celsius);
+	if ( scanf("%lf", &fahrenheit) == 1 )
+	{
+		celsius = 5.0 / 9.0 * (fahrenheit - 32.0);
+		printf("%lg Fahrenheit == %lg Celsius\n", fahrenheit, celsius);
+	}
 	return 0;
 }
--- tools/GdbTest/examples/ftoc_i.c
+++ tools/GdbTest/examples/ftoc_i.c
@@ -4,8 +4,10 @@ int main()
 {
 	double fahrenheit = 0, celsius = 0;
 	printf("Temperature (Fahrenheit): ");
-	scanf("%lf", &fahrenheit);
-	celsius = 5 / 9 * (fahrenheit - 32);
-	printf("%lg Fahrenheit == %lg Celsius\n", fahrenheit, celsius);
+	if ( scanf("%lf", &fahrenheit) == 1 )
+	{
+		celsius = 5 / 9 * (fahrenheit - 32);
+		printf("%lg Fahrenheit == %lg Celsius\n", fahrenheit, celsius);
+	}
 	return 0;
 }
