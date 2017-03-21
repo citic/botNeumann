e3d88823d14cfae6304e380ad0b62b67a795605b
Jeisson Hidalgo <jeissonh@gmail.com>
2017-03-21 16:16:43 -0600

    Fixed margin left at memory top's label

--- source/UnitPlaying/Memory/MemoryTop.cpp
+++ source/UnitPlaying/Memory/MemoryTop.cpp
@@ -44,7 +44,7 @@ void MemoryTop::buildLabel(const QString& labelText)
 	labelAssets << "up_variable_name_left" << "up_variable_name_middle" << "up_variable_name_right";
 
 	// Calculate proportions of each part of the tape, using the bytes they require
-	const qreal labelBytes = qMin( labelText.length() / 2.5, size - 2.0 );
+	const qreal labelBytes = qMin( labelText.length() / 2.85, qreal(size) );
 	const qreal leftRightProportion = 3.246 / 11.951 / labelBytes;
 	const qreal middleProportion =  1.0 - 2 * leftRightProportion;
 	QList<qreal> proportions;
@@ -53,11 +53,10 @@ void MemoryTop::buildLabel(const QString& labelText)
 	// Create the label and add it to the scene
 	label = new MultiSvgButton(labelAssets, proportions, graphicsParentItem, labelText, zValue + 0.01 );
 	const qreal refTopHeight = 21.737;
-	label->setMarginLeft(0.2);
 	label->setMarginTop( 1.128 / refTopHeight );
 	label->setMarginBottom( 5.756 / refTopHeight );
 	label->setFont(QFont(BotNeumannApp::getMonospacedFontName()));
 	label->setBrush(QBrush(Qt::black));
 	label->setShear(-0.2, 0.0);
-	addItem(label, labelBytes / size, zValue + 0.01);
+	insertItem(label, 1.25 / (size + 2.0), labelBytes / (size + 2.0), zValue + 0.01);
 }
