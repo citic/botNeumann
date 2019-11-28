aeac700a81110c06166c5940e6e53e1020b0d2c5
Jeisson Hidalgo <jeisson.hidalgo@ucr.ac.cr>
2019-11-28 11:00:31 -0600

    Fixed script for generating executables in ECCI-labs

--- tools/build4.sh
+++ tools/build4.sh
@@ -6,7 +6,7 @@ if [ -d $QT ]; then
     export PATH=$QT:$PATH
 else
     QT=/opt/Qt5.12.0/5.12.0/gcc_64/bin
-    export $QT:$PATH
+    export PATH=$QT:$PATH
 fi
 
 if [ "$(basename $(pwd))" = "tools" ]; then
@@ -16,8 +16,7 @@ fi
 mkdir -p build && cd build
 
 BASE='qmake ../../botNeumann.pro CONFIG+=release'
-mkdir -p ag && cd ag && $BASE DEFINES='ABSTRACT=1 GAMIFICATION=1' && make -j$CPU && cd ..
-mkdir -p as && cd as && $BASE DEFINES='ABSTRACT=1 GAMIFICATION=0' && make -j$CPU && cd ..
-mkdir -p cg && cd cg && $BASE DEFINES='ABSTRACT=0 GAMIFICATION=1' && make -j$CPU && cd ..
-mkdir -p cs && cd cs && $BASE DEFINES='ABSTRACT=0 GAMIFICATION=0' && make -j$CPU && cd ..
-
+mkdir -p ag3 && cd ag3 && $BASE DEFINES='ABSTRACT=1 GAMIFICATION=1' && make -j$CPU && cd ..
+mkdir -p as2 && cd as2 && $BASE DEFINES='ABSTRACT=1 GAMIFICATION=0' && make -j$CPU && cd ..
+mkdir -p cg1 && cd cg1 && $BASE DEFINES='ABSTRACT=0 GAMIFICATION=1' && make -j$CPU && cd ..
+mkdir -p cs4 && cd cs4 && $BASE DEFINES='ABSTRACT=0 GAMIFICATION=0' && make -j$CPU && cd ..
