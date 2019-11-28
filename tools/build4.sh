dcfb78e58c0d76a20c71358425f4c9b35b68f23b
Jeisson Hidalgo <jeisson.hidalgo@ucr.ac.cr>
2019-11-28 03:10:57 -0600

    Script for generating 4 experimental versions adapted to virtual machine

--- tools/build4.sh
+++ tools/build4.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-export PATH=/opt/Qt5.12.0/5.12.0/gcc_64/bin:$PATH
+CPU=$(nproc --all)
+QT=/opt/Qt5.13.1/5.13.1/gcc_64/bin
+if [ -d $QT ]; then
+    export PATH=$QT:$PATH
+else
+    QT=/opt/Qt5.12.0/5.12.0/gcc_64/bin
+    export $QT:$PATH
+fi
 
 if [ "$(basename $(pwd))" = "tools" ]; then
 	cd ..
@@ -8,7 +15,9 @@ fi
 
 mkdir -p build && cd build
 
-mkdir -p ag && cd ag && qmake ../../botNeumann.pro CONFIG+=release DEFINES='ABSTRACT=1 GAMIFICATION=1' && make -j4 && cd ..
-mkdir -p as && cd as && qmake ../../botNeumann.pro CONFIG+=release DEFINES='ABSTRACT=1 GAMIFICATION=0' && make -j4 && cd ..
-mkdir -p cg && cd cg && qmake ../../botNeumann.pro CONFIG+=release DEFINES='ABSTRACT=0 GAMIFICATION=1' && make -j4 && cd ..
-mkdir -p cs && cd cs && qmake ../../botNeumann.pro CONFIG+=release DEFINES='ABSTRACT=0 GAMIFICATION=0' && make -j4 && cd ..
+BASE='qmake ../../botNeumann.pro CONFIG+=release'
+mkdir -p ag && cd ag && $BASE DEFINES='ABSTRACT=1 GAMIFICATION=1' && make -j$CPU && cd ..
+mkdir -p as && cd as && $BASE DEFINES='ABSTRACT=1 GAMIFICATION=0' && make -j$CPU && cd ..
+mkdir -p cg && cd cg && $BASE DEFINES='ABSTRACT=0 GAMIFICATION=1' && make -j$CPU && cd ..
+mkdir -p cs && cd cs && $BASE DEFINES='ABSTRACT=0 GAMIFICATION=0' && make -j$CPU && cd ..
+
